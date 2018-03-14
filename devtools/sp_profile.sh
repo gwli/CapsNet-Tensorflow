f8ee5b71226f84716eb507401a03bfad64792e47
victor <vili@nvidia.com>
2018-03-14 12:17:38 +0800

    update docker_cmd to tensorflow-1.4 	modified:   docker_cmd.sh 	modified:   sp_profile.sh

--- devtools/sp_profile.sh
+++ devtools/sp_profile.sh
@@ -1,5 +1,6 @@
-sp="/raid/tools/SP/SystemProfiler-linux-public-3.9.3747-52a8955/Target-x86_64/x86_64/sp"
+sp="/raid/tools/SP/SystemProfiler-linux-public-4.0.1163-b6fa40c/Target-x86_64/x86_64/sp"
 cmd="python main.py"
 cd ..
 rm -fr ./*.qdstrm
-$sp profile --delay=10 --duration=10 -o hoomd_blue.qdstrm -t cuda,cublas,curand,pthread,cudnn $cmd
+$sp profile --delay=40 --duration=10 -o CapsNet-Tensorflow.qdstrm -t cuda,cublas,curand,system,cudnn $cmd
+mv CapsNet-Tensorflow.qdstrm ./devtools
